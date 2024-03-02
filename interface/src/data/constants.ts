83dbcd14b214e0edbd401a7f51afd6d4c6d70f95
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-03-02 09:07:11 -0700

    add more chains and dynamic contract records

--- interface/src/data/constants.ts
+++ interface/src/data/constants.ts
@@ -2,10 +2,40 @@ export const AUTO_CONNECT = "wagmi.connected";
 export const LAST_WALLET_CONNECTED = "wagmi.wallet";
 export const DISABLED_CHAINS = [1];
 
-// Base
-// export const FACTORY_CONTRACT = "0xf312F04DF2885Ec7C44cde77bAC8BB8B1FB4Aa20";
-// export const COUNTER_CONTRACT = "0x94789011AcF3c7411d1db9Abe8337f4BA7589D9f";
+export const factory_contract: Record<number, string> = {
+	// Arbitrum Sepolia
+	421614: "0xF6c8EED3bE03a482Fe5d43c4EFC330d14dB7cfA4",
+	// Base
+	8453: "0xf312F04DF2885Ec7C44cde77bAC8BB8B1FB4Aa20",
+	// Sepolia
+	11155111: "0x779638ff9c9df1810D08358490981DCAE34ed01a",
+	// Linea
+	59144: "0xF6c8EED3bE03a482Fe5d43c4EFC330d14dB7cfA4",
+	// XDC
+	50: "xdcf6c8eed3be03a482fe5d43c4efc330d14db7cfa4",
+	// Inco Test
+	9090: "0xF6c8EED3bE03a482Fe5d43c4EFC330d14dB7cfA4",
+	// inEVM Test
+	1234: "0xa52b8D81C2A8673C704a35557E6a8B8791fC3506",
+	// Fhenix Frontier
+	42069: "0xF6c8EED3bE03a482Fe5d43c4EFC330d14dB7cfA4",
+};
 
-// Sepolia
-export const FACTORY_CONTRACT = "0x779638ff9c9df1810D08358490981DCAE34ed01a";
-export const COUNTER_CONTRACT = "0x37df0c956cdcd13C0EF28fC72D33011001e98DA2";
+export const counter_contract: Record<number, string> = {
+	// Arbitrum Sepolia
+	421614: "0xf312F04DF2885Ec7C44cde77bAC8BB8B1FB4Aa20",
+	// Base
+	8453: "0x94789011AcF3c7411d1db9Abe8337f4BA7589D9f",
+	// Sepolia
+	11155111: "0x37df0c956cdcd13C0EF28fC72D33011001e98DA2",
+	// Linea
+	59144: "0xf312F04DF2885Ec7C44cde77bAC8BB8B1FB4Aa20",
+	// XDC
+	50: "0x94789011AcF3c7411d1db9Abe8337f4BA7589D9f",
+	// Inco Test
+	9090: "0xf312F04DF2885Ec7C44cde77bAC8BB8B1FB4Aa20",
+	// inEVM Test
+	1234: "0x144E52b74F837F5529b0c41e900BB5c61d30afb4",
+	// Fhenix Frontier
+	42069: "x94789011AcF3c7411d1db9Abe8337f4BA7589D9f",
+};
